cc257bc159acd7cf9a416533dda330ac73d4beb7
Gaëlle <marais.gaelle@gmail.com>
2018-12-10 14:51:27 +0100

    test index

--- asm/LC3_index.asm
+++ asm/LC3_index.asm
@@ -1,15 +1,15 @@
 ; @param R2 adresse du caractere 
 ; @param R1 adresse de la chaine
 ; @return R0 adresse de la premiere occurrence du caractère
-	.ORIG x3000
+	.ORIG x0000
 	LEA R1,chaine	; Chargement dans R1 de l'adresse de la chaîne
-	LD R2,caract	; Chargement dans R2 du code ASCII de 'l'
+	LD R2,caract	; Chargement dans R2 du code ASCII du caractere
 	AND R0,R0,0     ; Mise à 0 du compteur
 	NOT R2,R2       ; Calcul de l'opposé de R2
 	ADD R2,R2,1	    ; R2 = -R2
 loop:	LDR R3,R1,0     ; Chargement dans R3 du caractère pointé par R1
 	BRz fini        ; Test de fin de chaîne
-	ADD R3,R3,R2    ; Comparaison avec 'l'
+	ADD R3,R3,R2    ; Comparaison avec le caractere
 	BRnp suite      ; Non égalité
 	HALT
 suite:	ADD R1,R1,1	; Incrémentation du pointeur
@@ -19,5 +19,5 @@ suite:	ADD R1,R1,1	; Incrémentation du pointeur
 fini:	AND R0,R0,0
         HALT
 chaine:	.STRINGZ "Hello World"
-caract:	.STRINGZ "w"
+caract:	.STRINGZ "o"
 	.END
